f314a9ffb21f5d49de5427d38f68f462bc810221
pkumawat1994 <priyanshu.kumawat1007@gmail.com>
2022-07-29 10:48:40 +0530

    add code class crud with api

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Home from "./component/Home";
+import "./index.css";
+import MyReactTable from "./component/MyReactTable";
+import Toastify from "./component/Toastify";
+import Spinner from "./component/Spinner";
+import CrudAPI from "./component/CrudAPI";
+
+function App() {
+  return (
+    <>
+      // <MyReactTable />
+      // <Toastify />
+      // <Spinner />
+      <Home />
+      <CrudAPI />
+    </>
+  );
+}
+
+export default App;
---
+++ src/component/Home.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import Table from "./Table";
+
+export default class Home extends Component {
+  render() {
+    return (
+      <>
+        <Table />
+      </>
+    );
+  }
+}
